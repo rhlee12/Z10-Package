7d766e6d164c75374da01bc01d1597e5c00a5260
Robert Lee <rlee@neon-07707.eco.neoninternal.org>
2018-11-05 09:25:25 -0700

    updates from the weekend

--- Z10/R/daily_precip_stats.R
+++ Z10/R/daily_precip_stats.R
@@ -26,31 +26,59 @@
 #
 ##############################################################################################
 
-daily.precip.stats=function(site){
-
-    dp.id="DP1.00006.001"
-
-    avail=dp.avail(dp.id)
-
-    months=unlist(avail$months[avail$site==site])
-
-    all=lapply(months, function(m) get.data(dp.id = dp.id, site = site, month = m))
-
-    ALL=unlist(all, recursive = FALSE)
-
-    secondary=ALL[grepl(pattern = "SECPRE", x = names(ALL))]
-    primary=ALL[grepl(pattern = "PRIPRE", x = names(ALL))]
-
-    if(length(secondary)==0){secondary=list(temp=data.frame(endDateTime=NA, Bulk=NA))}
-    if(length(primary)==0){primary=list(temp=data.frame(endDateTime=NA, Bulk=NA))}
-
-    sec.df=data.frame(do.call(rbind, .common.fields(secondary)), row.names = NULL)
-    pri.df=data.frame(do.call(rbind, .common.fields(primary)), row.names = NULL)
-
-    both=list(primary=pri.df, secondary=sec.df)
-
-    out=lapply(both, function(x) .sum.do.basic.stats(x, field.key="Bulk"))
-
-    return(out)
+daily.precip.stats=function(site, bgn.date, end.date){
+  
+  dp.id="DP1.00006.001"
+  
+  avail=dp.avail(dp.id)
+  
+  months=unlist(avail$months[avail$site==site])
+  
+  if(missing(bgn.date)){
+    bgn.date=paste0(months[1], "-01")
+  }
+  
+  if(missing(end.date)){
+    end.month=months[length(months)]
+    last.day=lubridate::days_in_month(as.Date(paste0(end.month, "-01")))
+    
+    end.date=paste0(end.month, "-", last.day)
+  }
+  
+  req.months=substr(seq.Date(from=as.Date(bgn.date), to=as.Date(end.date), by = '1 month'), start = 1, stop = 7)
+  
+  months=req.months[req.months %in% months]
+  
+  date.seq=seq.Date(from=as.Date(bgn.date), to=as.Date(end.date), by = "1 day")
+  
+  all=lapply(months, function(m) get.data(dp.id = dp.id, site = site, month = m))
+  
+  ALL=unlist(all, recursive = FALSE)
+  
+  secondary=ALL[grepl(pattern = "SECPRE", x = names(ALL))]
+  primary=ALL[grepl(pattern = "PRIPRE", x = names(ALL))]
+  
+  if(length(secondary)==0){secondary=list(temp=data.frame(endDateTime=NA, Bulk=NA))}
+  if(length(primary)==0){primary=list(temp=data.frame(endDateTime=NA, Bulk=NA))}
+  
+  sec.df=data.frame(do.call(rbind, .common.fields(secondary)), row.names = NULL)
+  pri.df=data.frame(do.call(rbind, .common.fields(primary)), row.names = NULL)
+  
+  both=list(primary=pri.df, secondary=sec.df)
+  
+  both=lapply(both, function(d) .dayify(d))
+  
+  temp=lapply(both, 
+              function(x) unlist(lapply(date.seq, 
+                                        function(d) mean(x[as.character(x$date)==d,grepl(pattern = "Bulk", x = colnames(x))], na.rm=T))
+              )
+  )
+  
+  out=temp
+  
+  out=data.frame(date.seq, data.frame(do.call(cbind, out)))
+  
+  return(out)
 }
+
 
--- Z10/R/helper_functions.R
+++ Z10/R/helper_functions.R
@@ -1,53 +1,53 @@
 .camel.to.dot=function(string){
   substring(text=string, first=regexec(pattern="[A-Z]", text=string, ignore.case = FALSE)[[1]][1])
-
-
-    dot.string=paste0(
+  
+  
+  dot.string=paste0(
     unlist(stringr::str_split(string, pattern = "[A-Z]")),
     ".",
     tolower(unlist(stringr::str_extract_all(string, pattern = "[A-Z]"))),
     collapse = "")
-
-    dot.string=substr(x=dot.string, start=1, stop = (nchar(dot.string)-2))
-
+  
+  dot.string=substr(x=dot.string, start=1, stop = (nchar(dot.string)-2))
+  
   return(dot.string)
 }
 
 .handle.dates=function(date.time){
-    out=as.POSIXct(gsub(x = date.time, pattern = "T|Z", replacement = " "), tz="UTC")
-    return(out)
+  out=as.POSIXct(gsub(x = date.time, pattern = "T|Z", replacement = " "), tz="UTC")
+  return(out)
 }
 
 .common.fields=function(df.list){
-    temp=unique(lapply(df.list, colnames))
-    if(length(temp)>1){
+  temp=unique(lapply(df.list, colnames))
+  if(length(temp)>1){
     common=do.call(dplyr::intersect, temp)
     out=lapply(df.list, function(x) x[,which(colnames(x) %in% common)])
-    }else{out=df.list}
-    return(out)
+  }else{out=df.list}
+  return(out)
 }
 
 .sum.do.basic.stats=function(x, field.key){
   merged.df=x
-
+  
   min=NA
   max=NA
   mean=NA
-
+  
   site.meta=get.site.meta(site)
-
+  
   if(nrow(merged.df)>1){
-
+    
     merged.df$endDateTime=.handle.dates(merged.df$endDateTime)
-
+    
     merged.df$endDateTime=lubridate::with_tz(merged.df$endDateTime, tzone = site.meta$location.properties$locationPropertyValue[site.meta$location.properties$locationPropertyName=="Value for Site Timezone"])
-
+    
     merged.df$day= cut(x=merged.df$endDateTime, breaks = "1 day")
-
+    
     mean=mean(unlist(lapply(unique(merged.df$day), function(d) sum(merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))], na.rm = T))))
     min=min(unlist(lapply(unique(merged.df$day), function(d) sum(merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))], na.rm = T))))
     max=max(unlist(lapply(unique(merged.df$day), function(d) sum(merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))], na.rm = T))))
-
+    
     out=c("minimum"=min, "mean"=mean, "maximum"=max)
   }
   return(c("minimum"=min, "mean"=mean, "maximum"=max))
@@ -55,28 +55,35 @@
 
 .do.basic.stats=function(x, field.key){
   merged.df=x
-
+  
   min=NA
   max=NA
   mean=NA
-
+  
   site.meta=get.site.meta(site)
-
+  
   if(nrow(merged.df)>1){
-
+    
     merged.df$endDateTime=.handle.dates(merged.df$endDateTime)
-
+    
     merged.df$endDateTime=lubridate::with_tz(merged.df$endDateTime, tzone = site.meta$location.properties$locationPropertyValue[site.meta$location.properties$locationPropertyName=="Value for Site Timezone"])
-
+    
     merged.df$day= cut(x=merged.df$endDateTime, breaks = "1 day")
-
+    
     mean=mean(unlist(lapply(unique(merged.df$day), function(d) merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))])), na.rm = T)
     min=min(unlist(lapply(unique(merged.df$day), function(d) merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))])), na.rm = T)
     max=max(unlist(lapply(unique(merged.df$day), function(d) merged.df[merged.df$day==d, grepl(pattern = field.key, x = colnames(merged.df))])), na.rm = T)
-
+    
     out=c("minimum"=min, "mean"=mean, "maximum"=max)
   }
   return(c("minimum"=min, "mean"=mean, "maximum"=max))
+}
+
+.dayify=function(df){
+  df$endDateTime=as.POSIXct(x = gsub(pattern = "T|Z", replacement = " ", x = df$endDateTime), format="%Y-%m-%d %H:%M:%S")
+  df$date=cut.Date(x=as.Date(df$endDateTime), breaks = "1 day")
+  
+  return(df)
 }
 
 
